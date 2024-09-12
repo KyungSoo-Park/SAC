7db2a2c1bad0877434de6e8220ec6687bfb16c70
KyungSoo-Park <51601627+KyungSoo-Park@users.noreply.github.com>
2024-09-12 13:23:20 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 (function () {
   const template = document.createElement('template') 
   template.innerHTML = `
-      <styte>
+      <style>
       </style>
       <div id="root" style="width: 100%; height: 100%;"> 
       Hello Custom Widget
       </div>
   `
   class Main extends HTMLElement {
-    
+
+    constructor () {
+      super()
+  
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(template.content.cloneNode(true)) 
+      
+      this._root = this._shadowRoot.getElementById('root')
+    }
     onCustomWidgetResize (width, height) {
       this.render()
       }
@@ -22,15 +30,6 @@
       render () {
       this._root.textcontent = 'Hello Custom Widget clientwidth: ${this.clientwidth}, clientHeight: ${this.clientHeight}' }
       
-    constructor () {
-      super()
-  
-      this._shadowRoot = this.attachShadow({ mode: 'open' })
-      this._shadowRoot.appendChild(template.content.cloneNode(true)) 
-      
-      this._root = this._shadowRoot.getElementById('root')
-    }
-
   }
 
   customElements.define('com-sap-sac-exercise-seamark-main', Main)
