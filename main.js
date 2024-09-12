4d924b988a2ea1852da640072e64a68a9cfbd7a0
KyungSoo-Park <51601627+KyungSoo-Park@users.noreply.github.com>
2024-09-12 14:28:32 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+var getScriptPromisify = (src) => {
+  return new Promise((resolve) => {
+    $.getScript(src, resolve)
+  })
+}
+
+var parseMetadata = metadata => {
+  const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata
+  const dimensions = []
+  for (const key in dimensionsMap) {
+    const dimension = dimensionsMap[key]
+    dimensions.push({ key, ...dimension })
+  }
+  const measures = []
+  for (const key in measuresMap) {
+    const measure = measuresMap[key]
+    measures.push({ key, ...measure })
+  }
+  return { dimensions, measures, dimensionsMap, measuresMap }
+}
 
 (function () {
   const template = document.createElement('template') 
@@ -18,6 +38,7 @@
       this._shadowRoot.appendChild(template.content.cloneNode(true)) 
       
       this._root = this._shadowRoot.getElementById('root')
+      this._eChart = null
     }
 
     onCustomWidgetResize (width, height) {
@@ -30,14 +51,47 @@
   
   onCustomWidgetDestroy () { 
   }
-    async render () {
-      const dataBinding = this.dataBinding 
-      if (!dataBinding || dataBinding.state !== 'success' ) {
-          return
-      }
-      this._root.textContent = JSON.stringify(dataBinding) 
-  }
 
+    async render () {
+      const dataBinding = this.dataBinding
+      if (!dataBinding || dataBinding.state !== 'success') { return }
+
+      await getScriptPromisify('https://echarts.apache.org/examples/en/editor.html?c=line-simple')
+
+      const { data, metadata } = dataBinding
+      const { dimensions, measures } = parseMetadata(metadata)
+      // dimension
+      const categoryData = []
+      // measures
+      const series = measures.map(measure => {
+        return {
+          id: measure.id,
+          name: measure.label,
+          data: [],
+          key: measure.key,
+          type: 'line',
+          smooth: true
+        }
+      })
+      data.forEach(row => {
+        categoryData.push(dimensions.map(dimension => {
+          return row[dimension.key].label
+        }).join('/')) // dimension
+        series.forEach(series => {
+          series.data.push(row[series.key].raw)
+        }) // measures
+      })
+
+      if (this._eChart) { echarts.dispose(this._eChart) }
+      const eChart = this._eChart = echarts.init(this._root, 'main')
+      const option = {
+        xAxis: { type: 'category', data: categoryData },
+        yAxis: { type: 'value' },
+        tooltip: { trigger: 'axis' },
+        series
+      }
+      eChart.setOption(option)
+    }
   }
 
   customElements.define('com-sap-sac-exercise-seamark-main', Main)
