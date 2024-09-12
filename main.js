33bf4f95b2e3d2e3273375a378ace3812c82c09a
KyungSoo-Park <51601627+KyungSoo-Park@users.noreply.github.com>
2024-09-12 14:16:01 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 (function () {
   const template = document.createElement('template') 
   template.innerHTML = `
@@ -14,7 +15,7 @@
       super()
   
       this._shadowRoot = this.attachShadow({ mode: 'open' })
-      this._shadowRoot.appendChild(template.content.cloneNode(true)) 
+      this._shadowRoot.appendChild(template.content.cloneNode(true))
       
       this._root = this._shadowRoot.getElementById('root')
     }
@@ -31,8 +32,8 @@
   }
     async render () {
       const dataBinding = this.dataBinding 
-      if (!dataBinding || dataBinding.state !== 'success' ) {
-          return
+      if (!dataBinding || dataBinding.state !== 'success') {
+          return                                              
       }
       this._root.textContent = JSON.stringify(dataBinding) 
   }
@@ -40,5 +41,6 @@
   }
 
   customElements.define('com-sap-sac-exercise-seamark-main', Main)
+    
 
 })()
